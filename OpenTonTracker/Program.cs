077d3047935fd47516e8cd4bf43f2e0b7b634735
Ludwintor <izumrud233@gmail.com>
2024-01-15 00:03:56 +0300

    Workaround dedust pool update delay
    Even that trades are fetched first, pool data can be still old so now it should be ok now

--- OpenTonTracker/Program.cs
+++ OpenTonTracker/Program.cs
@@ -17,9 +17,15 @@ namespace OpenTonTracker
         private const string OPEN_ADDRESS = "EQDf84FT8tdHZeI2-LXdb8gPMRqHRSABrmi8jI7MzvVpGJKZ";
         private const string TONVIEWER = "https://tonviewer.com/";
         private const int MS_DELAY = 10000;
+        private const int RETRY_DELAY = 133;
+        private const int PRICE_CHANGE_RETRIES = 3;
 
         private static readonly EventId _botEvent = new(420, "OpenPrice");
+#if !DEBUG
         private static readonly ChatId _channelPriceId = new(-1002056517262);
+#else
+        private static readonly ChatId _channelPriceId = new(-1002070106680); // test channel
+#endif
         private static readonly InlineKeyboardMarkup _urls;
 
         private static TelegramBotClient _botClient = null!;
@@ -37,7 +43,7 @@ namespace OpenTonTracker
                 },
                 new[]
                 {
-                    InlineKeyboardButton.WithUrl("Buy on Dedust", "https://dedust.io/swap/TON/EQDf84FT8tdHZeI2-LXdb8gPMRqHRSABrmi8jI7MzvVpGJKZ")
+                    InlineKeyboardButton.WithUrl("Buy on Dedust", "https://dedust.io/swap/TON/OPEN")
                 }
             });
         }
@@ -51,7 +57,7 @@ namespace OpenTonTracker
             else
                 token = Environment.GetEnvironmentVariable(BOT_TOKEN_ENV);
             if (string.IsNullOrEmpty(token))
-                throw new ArgumentNullException(nameof(token), $"Set telegram bot token to environment variable \"{BOT_TOKEN_ENV}\" or provide it as first argument");
+                throw new ArgumentNullException(nameof(token), $"Set telegram bot token to environment variable \"{BOT_TOKEN_ENV}\" or provide it as first argument (argument in high priority)");
             _botClient = new(token);
             Run().GetAwaiter().GetResult();
         }
@@ -66,7 +72,6 @@ namespace OpenTonTracker
                 try
                 {
                     Trade[] trades = await _dedustClient.FetchPoolTradesAsync(OPENTON_POOL, 1);
-                    await Task.Delay(200);
                     Pool[] pools = await _dedustClient.FetchPoolsAsync();
                     Pool openTonPool = pools.Where(x => x.Address == OPENTON_POOL).First();
                     _lastTonPrice = openTonPool.CalculateRightToLeft(1d, null, 5);
@@ -81,15 +86,15 @@ namespace OpenTonTracker
             while (true)
             {
                 sb.Clear();
-                Trade[]? trades = null;
-                Pool? tonUsdtPool = null;
-                Pool? openTonPool = null;
+                Trade[] trades = null!;
+                Pool tonUsdtPool = default;
+                Pool openTonPool = default;
                 try
                 {
                     trades = await _dedustClient.FetchPoolTradesAsync(OPENTON_POOL, 5, lastLt);
                     if (trades.Length > 0)
                     {
-                        await Task.Delay(200);
+                        await Task.Delay(250);
                         Pool[] pools = await _dedustClient.FetchPoolsAsync();
                         tonUsdtPool = pools.Where(x => x.Address == USDTTON_POOL).First();
                         openTonPool = pools.Where(x => x.Address == OPENTON_POOL).First();
@@ -98,30 +103,58 @@ namespace OpenTonTracker
                 catch (Exception ex) when (ex is HttpRequestException || ex is TaskCanceledException)
                 {
                     _dedustClient.Logger.LogError(_botEvent, ex, "Unable to fetch last trades. Retrying after {Seconds} seconds", MS_DELAY / 1000);
+                    await Task.Delay(MS_DELAY);
+                    continue;
                 }
-                if (trades != null && trades.Length > 0)
+                catch (InvalidOperationException ex)
+                {
+                    _dedustClient.Logger.LogError(_botEvent, ex, "Unable to find TON/jUSDT pool or TON/OPEN pool. Retrying after {Seconds} seconds", MS_DELAY / 1000);
+                    await Task.Delay(MS_DELAY);
+                    continue;
+                }
+                if (trades.Length > 0)
                 {
                     tradeWriter.WriteTrades(trades, sb, tonUsdtPool);
-                    if (openTonPool != null)
-                    {
-                        double tonPrice = openTonPool.Value.CalculateRightToLeft(1d, null, 5);
-                        double usdtPrice = tonUsdtPool?.CalculateLeftToRight(tonPrice) ?? 0d;
-                        double priceChange = _lastTonPrice != 0d ? tonPrice / _lastTonPrice - 1d : 0d;
-                        bool changeUp = priceChange >= 0d;
-                        Rune changeEmoji = changeUp ? Emojis.UptrendChart : Emojis.DowntrendChart;
-                        sb.AppendLine().Append(Emojis.BarChart.ToString()).Append("Price: ")
-                          .Append(Utils.EscapeMarkdown(tonPrice.ToString("0.000000")))
-                          .Append(" TON \\($")
-                          .Append(Utils.EscapeMarkdown(usdtPrice.ToString("0.000000")))
-                          .Append("\\) ").Append(changeEmoji.ToString()).Append(" \\")
-                          .Append(changeUp ? '+' : '-').Append(Utils.EscapeMarkdown(Math.Abs(priceChange * 100d).ToString("0.00"))).Append('%');
-                        _lastTonPrice = tonPrice;
-                    }
+                    double tonPrice = await RetrievePrice(openTonPool);
+                    double usdtPrice = tonUsdtPool.CalculateLeftToRight(tonPrice);
+                    double priceChange = tonPrice / _lastTonPrice - 1d;
+                    bool changeUp = priceChange >= 0d;
+                    Rune changeEmoji = changeUp ? Emojis.UptrendChart : Emojis.DowntrendChart;
+                    sb.AppendLine().Append(Emojis.BarChart.ToString()).Append("Price: ")
+                        .Append(Utils.EscapeMarkdown(tonPrice.ToString("0.000000")))
+                        .Append(" TON \\($")
+                        .Append(Utils.EscapeMarkdown(usdtPrice.ToString("0.000000")))
+                        .Append("\\) ").Append(changeEmoji.ToString()).Append(" \\")
+                        .Append(changeUp ? '+' : '-').Append(Utils.EscapeMarkdown(Math.Abs(priceChange * 100d).ToString("0.00"))).Append('%');
+                    _lastTonPrice = tonPrice;
                     lastLt = trades[^1].Lt;
                     await _botClient.SendTextMessageAsync(_channelPriceId, sb.ToString(), disableWebPagePreview: true, parseMode: Telegram.Bot.Types.Enums.ParseMode.MarkdownV2, replyMarkup: _urls);
                 }
                 await Task.Delay(MS_DELAY);
             }
+        }
+
+        private static async ValueTask<double> RetrievePrice(Pool tokenTonPool)
+        {
+            double tonPrice = tokenTonPool.CalculateRightToLeft(1d, null, 5);
+            int retries = 0;
+            while (tonPrice == _lastTonPrice && retries < PRICE_CHANGE_RETRIES)
+            {
+                await Task.Delay(RETRY_DELAY);
+                try
+                {
+                    Pool[] pools = await _dedustClient.FetchPoolsAsync();
+                    tokenTonPool = pools.Where(x => x.Address == OPENTON_POOL).First();
+                }
+                catch (Exception ex)
+                {
+                    _dedustClient.Logger.LogError(_botEvent, ex, "Unable to retrieve pools durting price retrieve retry. Retrying after {Seconds} seconds", (RETRY_DELAY / 1000d).ToString("0.00"));
+                    continue;
+                }
+                tonPrice = tokenTonPool.CalculateRightToLeft(1d, null, 5);
+                retries++;
+            }
+            return tonPrice;
         }
     }
 }
